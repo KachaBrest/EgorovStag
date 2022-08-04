f4e805e89a4f5ef4bb2fd942d0bef93c226a2972
KachaBrest <kacha.brest@gmail.com>
2022-08-04 11:18:36 +0300

    'link'

--- lib/post_link.php
+++ lib/post_link.php
@@ -16,7 +16,22 @@ $psts  = get_posts( array(
 
 foreach( $psts as $pst ){
     if($pst->post_name == $link) {
+
+        // начало эксперемента
+
+        $cur_terms = get_the_terms( $pst->ID, 'destination' );
+        if( is_array( $cur_terms ) ){
+            echo '<div class="services">';
+            foreach( $cur_terms as $cur_term ){
+                echo '<a href="'. get_term_link( $cur_term->term_id, $cur_term->taxonomy ) .'">'. $cur_term->name .'</a>';
+            }
+            echo '</div>';
+        }
+
+        //конец эксперемента
+
         echo '<h1>'. $pst->post_title .'</h1>';
         echo '<div class="link_content">'. $pst->post_content . '</div>';
+
     }
 }
